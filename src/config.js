9bdf55261ecb49733857228546994870f8ed9d15
agutowski-misterioit <gutowski007@gmail.com>
2021-12-30 15:53:36 +0100

    change .gitignore and delete files from commit

--- src/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET = exports.APIKEY = void 0;
-const dotenv = require("dotenv");
-dotenv.config();
-exports.APIKEY = process.env.APIKEY;
-exports.JWT_SECRET = process.env.JWT_SECRET;
